1a161a2e09c8dc5c0b5e624cc834ca634ef1d2b1
Danilo  Marques <125525104+Danilomarques02@users.noreply.github.com>
2023-09-21 20:08:20 -0300

    update

--- revisao-logica/tipos-primirivos.js
+++ revisao-logica/tipos-primirivos.js
@@ -13,3 +13,10 @@ let msg = `eu possuo ${idade} anos`;
 
 console.log(msg);
 console.log("Hello " + "world")
+console.log(typeof msg, typeof idade, typeof minhaVar, " -- ", 20);
+
+
+const n1 = 10;
+const n2 = 1.1;
+console.log(`O tipo de n1 é ${typeof n1} e seu valor é ${n1}`);
+console.log(`O tipo de n2 é ${typeof n2} e seu valor é ${n2}`);
