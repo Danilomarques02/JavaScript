b91475a0c42256db0c9849f247ec0365248023a7
Danilo  Marques <125525104+Danilomarques02@users.noreply.github.com>
2023-09-21 20:46:36 -0300

    Update

--- revisao-logica/conversao-tipos.js
+++ revisao-logica/conversao-tipos.js
@@ -6,3 +6,10 @@ let n2 = 2;
 console.log(n1 + n2, typeof n1, typeof n2, n1);
 
 //parseInt, parseFloat, Number()
+
+//decimais 0 - 9
+//hexadecimais 0 - f (15)
+
+n2 = 15;
+n2 = n2.toString(2);
+console.log(n2, typeof n2);
