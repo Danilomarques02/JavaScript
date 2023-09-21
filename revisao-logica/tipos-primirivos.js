eb5a2617e6209aeb0fb7e7e572ba109103e4dd57
Danilo  Marques <125525104+Danilomarques02@users.noreply.github.com>
2023-09-21 19:43:56 -0300

    create tipos-primitivos

---
+++ revisao-logica/tipos-primirivos.js
@@ -0,0 +1,21 @@
+//var, let, const
+
+//var -> Ess - cross-browser
+//let e const -> ES2015
+
+
+let teste = "minha string";
+teste = 10;
+
+const teste2 =10;
+
+let teste3;
+
+console.log(teste);
+
+teste3 =  20;
+console.log(teste3);
+
+const teste4 ="eu sou imutavel";
+console.log(teste4);
+
