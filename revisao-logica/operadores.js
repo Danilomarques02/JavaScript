646c49e6c892bf97dd86820df1a25e2145af3a96
Danilo  Marques <125525104+Danilomarques02@users.noreply.github.com>
2023-09-21 20:57:37 -0300

    Create operadores

---
+++ revisao-logica/operadores.js
@@ -0,0 +1,15 @@
+// + - * / % ** 
+
+let n1 = 10;
+let n2 = 5;
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
+console.log(n1 % n2);
+console.log(2 ** 3);
+
+let n3 = 21;
+// n3 = n3 * 2;
+n3%=2;
+console.log(n3);
