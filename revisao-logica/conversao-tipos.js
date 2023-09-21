936768e6182873920f95b4f20aaf72ac48b050d3
Danilo  Marques <125525104+Danilomarques02@users.noreply.github.com>
2023-09-21 20:40:52 -0300

    create conversao-tipo

---
+++ revisao-logica/conversao-tipos.js
@@ -0,0 +1,8 @@
+let n1 = "oi";
+//n1 = parseInt(n1)
+
+let n2 = 2;
+
+console.log(n1 + n2, typeof n1, typeof n2, n1);
+
+//parseInt, parseFloat, Number()
--- revisao-logica/tipos-primitivos.js
+++ revisao-logica/tipos-primitivos.js
@@ -1,4 +1,6 @@
 //string, nomber (int, floats), boolean 
+// undefined, null, symbol(ES2015)
+
 
 let minhaVar = "minha string"
 let minhavar2 = "minha 'string' com aspas duplas";
@@ -20,3 +22,14 @@ const n1 = 10;
 const n2 = 1.1;
 console.log(`O tipo de n1 é ${typeof n1} e seu valor é ${n1}`);
 console.log(`O tipo de n2 é ${typeof n2} e seu valor é ${n2}`);
+
+
+const isValid = false;
+console.log(`isValid: ${isValid}`);
+
+let varTeste = null;
+console.log(varTeste);
+console.log(typeof varTeste);
+varTeste = 10;
+console.log(typeof varTeste, varTeste);
+
