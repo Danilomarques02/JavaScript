db78cdc1bcf0641ac84a6710f14ace63972ce5b7
Danilo  Marques <125525104+Danilomarques02@users.noreply.github.com>
2023-09-22 19:48:00 -0300

    Update operadores.js

--- revisao-logica/operadores.js
+++ revisao-logica/operadores.js
@@ -38,3 +38,20 @@ console.log(n1, n2, n3);
 console.log(n1 != "10");
 console.log(n1 !== "10");
 
+//OPERADORES LÓGICOS 
+/*
+Para uma pessoa viajar para o exterior:
+- precisar ser maior de 18 anos
+OU 
+Acompanhada com os pais 
+E 
+ter comprado bilhete
+*/
+
+let idade = 21;
+let paisPresentes = false;
+let comprouBilhete = false;
+const podeViajar =idade >= 18 || (paisPresentes && comprouBilhete );
+console.log(`Pode viajar: ${podeViajar}`);
+
+
