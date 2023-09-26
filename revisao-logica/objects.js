5fa248bd5c0364d0e9618e25677bdf176388e069
Danilo  Marques <125525104+Danilomarques02@users.noreply.github.com>
2023-09-26 20:43:04 -0300

    create objects.js

---
+++ revisao-logica/objects.js
@@ -0,0 +1,22 @@
+//const nomes = ["Danilo","Maria", "João"]
+//const idades = [null,21, 28,35]
+//console.log(nomes[0], idades[0])
+
+   /* const pessoa = new Object()
+    pessoa.nome ="Danilo"
+    pessoa.idade = 21
+    let prop = "nome"
+    console.log(pessoa[prop])
+    console.log(pessoa["idade"])
+    */
+
+const pessoa = {
+    nome:"Danilo",
+    idade: 40
+ 
+}
+pessoa.nome ="Danilo"
+pessoa.idade = 21
+let prop = "nome"
+console.log(pessoa[prop])
+console.log(pessoa["idade"])
