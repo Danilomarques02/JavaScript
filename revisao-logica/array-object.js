469cac196abeb6295991b6bd0afa1895b4bc5719
Danilo  Marques <125525104+Danilomarques02@users.noreply.github.com>
2023-10-03 19:40:21 -0300

    Create array.js

---
+++ revisao-logica/array-object.js
@@ -0,0 +1,15 @@
+const nomes= ["Danilo", "Maria", ""]
+const pessoa = {nome: "Danilo", idade: 10, email: ""}
+const pessoa1 ={}
+
+const pessoas = [{
+    nome:"Danilo",
+    idada: 21
+}, {
+    nome:"Helena",
+    idada: 45
+}, {
+    nome:"João",
+    idada: 30
+}]
+console.log(pessoas[0].nome)
