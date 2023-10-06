d60b3c2b55b68e7373c490a94bbd6ddd73b33e6c
Danilo  Marques <125525104+Danilomarques02@users.noreply.github.com>
2023-10-06 20:52:14 -0300

    Update

--- introducao/primeiro-exemplo.js
+++ introducao/primeiro-exemplo.js
@@ -1,1 +1,16 @@
 console.log("Javascript funcionando");
+
+
+//let nome = "Danilo"
+//console.log(10 + 20)
+
+console.log("qualquer coisas")
+
+//uma unica linha 
+
+let nome = "Danilo"
+alert(nome)
+
+const hasAttrubite = ""
+
+
